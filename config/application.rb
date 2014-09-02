d48d5d9aaa50571cbeae39e413fa487e93000207
morganric <morganric@gmail.com>
2014-09-02 16:33:27 +0100

    about

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Phunder
     end
 
     config.assets.precompile += %w( yeti.css yeti.js )
+    config.force_ssl = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
